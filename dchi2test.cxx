dcc66249739f0b3e6d37852d67a39865c7aecaca
Jesse Mendez <jessepmendez79@gmail.com>
2026-05-15 17:04:06 -0500

    Validated chi2 calculation

--- dchi2test.cxx
+++ dchi2test.cxx
@@ -11,53 +11,14 @@ using namespace std;
 
 #include "WCPLEEANA/TOsc.h"
 
+#include "TTreeReader.h"
+#include "TTreeReaderValue.h"
 #include "WCPLEEANA/Configure_Osc.h"
 
 #include "TApplication.h"
 #include "TParameter.h"
 #include <chrono> // timer
 
-////////////////////////////////// SUB
-////////////////////////////////////////////////
-void get_CL_curve(TH2 *h2_CL_input, TGraph *gh_CL_curve, int flag_index)
-{
-  gROOT->SetBatch( 1 );
-
-  TString roostr = "";
-
-  roostr = TString::Format("canv_list_95_%04d", flag_index);
-  TCanvas *canv_list_95 = new TCanvas(roostr, roostr, 800, 600);
-  cout<<endl<<" ---> "<<roostr<<endl;
-
-  double user_contours_95[1] = {0.95};
-  //double user_contours_95[1] = {0.683};
-  h2_CL_input->SetContour(1, user_contours_95);
-
-  // Draw contours as filled regions, and Save points
-  h2_CL_input->Draw("CONT Z LIST");
-  canv_list_95->Update();
-
-  // Get Contours
-  TObjArray *conts = (TObjArray*)gROOT->GetListOfSpecials()->FindObject("contours");
-
-  if (!conts){
-    printf("*** No Contours Were Extracted!\n");
-  }
-
-  TList* contLevel = nullptr;
-  TGraph* curv     = nullptr;
-
-  Int_t TotalConts = conts->GetSize();
-
-  printf("TotalConts = %d\n", TotalConts);
-
-  for(int i = 0; i < TotalConts; i++){
-    contLevel = (TList*)conts->At(i);
-    printf("Contour %d has %d Graphs\n", i, contLevel->GetSize());
-  }
-}
-
-std::vector<TGraph*> GetContourGraphs(TH2* h2, double level);
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////// MAIN //////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -66,26 +27,24 @@ int main(int argc, char** argv)
   TString roostr = "";
 
   cout<<endl<<" ---> A Hello story ..."<<endl<<endl;
-  int analysis = 0;
-int contour = 0;
-  int ifile = -1;
-  int obs_throw = 0;
-  int fcls = 0;
+
+
+
+
   double scaleF_POT_BNB  = 1;
   double scaleF_POT_NuMI = 1;
   int display = 0;
 
 
-  int sensitivity_study = 0;
   int it14 = 0;
   int idm2 = 0;
-  int it24 = 0;
+
 
   const int NUM_dm2 = 60;
   const int NUM_ttt = 60;
 
-  TH1D *h1d_dm2 = new TH1D("h1d_dm2", "h1d_dm2", NUM_dm2, -1, 2);
-  TH1D *h1d_ttt = new TH1D("h1d_ttt", "h1d_ttt", NUM_ttt, -3, 0);
+  TH1D *h1d_dm2 = new TH1D("h1d_dm2", "h1d_dm2", NUM_dm2, -2, 1);
+  TH1D *h1d_ttt = new TH1D("h1d_ttt", "h1d_ttt", NUM_ttt, -2, 0);
 
 
 
@@ -93,66 +52,6 @@ int contour = 0;
   // double idm2 = 0;
   // double it24 = 0;
 
-  for(int i=1; i<argc; i++) {
-    if( strcmp(argv[i],"-ifile")==0 ) {
-      stringstream convert( argv[i+1] );
-      if(  !( convert>>ifile ) ) { cerr<<" ---> Error ifile !"<<endl; exit(1); }
-    }
-    if( strcmp(argv[i],"-fobsthrow")==0 ) {
-      stringstream convert( argv[i+1] );
-      if(  !( convert>>obs_throw ) ) { cerr<<" ---> Error ifile !"<<endl; exit(1); }
-    }
-    if( strcmp(argv[i],"-fcls")==0 ) {
-      stringstream convert( argv[i+1] );
-      if(  !( convert>>fcls ) ) { cerr<<" ---> Error ifile !"<<endl; exit(1); }
-    }
-    if (strcmp(argv[i], "-option") == 0) {
-      if (strcmp(argv[i+1], "analysis") == 0) {
-        analysis = 1;
-        // cout << "ANALYSIS MODE \n";
-      }
-      if (strcmp(argv[i+1], "contour") == 0) {
-        contour = 1;
-        // cout << "ANALYSIS MODE \n";
-                }
-    }
-    if (strcmp(argv[i], "-flag") == 0) {
-      if (strcmp(argv[i+1], "sens") == 0) {
-        sensitivity_study = 1;
-        // cout << "ANALYSIS MODE \n";
-      }
-      // if (strcmp(argv[i+1], "xfile") == 0) {
-      //   xthrow = 1;
-      //   // cout << "ANALYSIS MODE \n";
-      // }
-    }
-    if( strcmp(argv[i],"-pbnb")==0 ) {
-      stringstream convert( argv[i+1] );
-      if(  !( convert>>scaleF_POT_BNB ) ) { cerr<<" ---> Error scaleF_POT_BNB !"<<endl; exit(1); }
-    }
-    if( strcmp(argv[i],"-pnumi")==0 ) {
-      stringstream convert( argv[i+1] );
-      if(  !( convert>>scaleF_POT_NuMI ) ) { cerr<<" ---> Error scaleF_POT_NuMI !"<<endl; exit(1); }
-    }
-    if( strcmp(argv[i],"-d")==0 ) {
-      stringstream convert( argv[i+1] );
-      if(  !( convert>>display ) ) { cerr<<" ---> Error display !"<<endl; exit(1); }
-    }
-    if( strcmp(argv[i],"-it14")==0 ) {
-      stringstream convert( argv[i+1] );
-      if(  !( convert>>it14 ) ) { cerr<<" ---> Error it14 !"<<endl; exit(1); }
-    }
-    if( strcmp(argv[i],"-idm2")==0 ) {
-      stringstream convert( argv[i+1] );
-      if(  !( convert>>idm2 ) ) { cerr<<" ---> Error idm2 !"<<endl; exit(1); }
-    }
-    if( strcmp(argv[i],"-it24")==0 ) {
-      stringstream convert( argv[i+1] );
-      if(  !( convert>>it24 ) ) { cerr<<" ---> Error it24 !"<<endl; exit(1); }
-    }
-  }
-
-  ///////////////////////////////////////////////////////////
 
   if( !display ) {
     gROOT->SetBatch( 1 );
@@ -162,7 +61,7 @@ int contour = 0;
 
   /////////////////////////////////////////////////////////// Draw style
 
-
+
   ///////////////////////////////////////////////////////////
 
   TOsc *osc_test = new TOsc();
@@ -250,24 +149,9 @@ int contour = 0;
                                Configure_Osc::default_fluxXs_dir,
                                Configure_Osc::default_detector_dir);
   osc_test->Set_oscillation_base(Configure_Osc::default_eventlist_dir);
+
+
   cout << "Read in xiangpan file \n";
-// Create structures to hold measurement and prediction
-  vector<double> vec_meas;
-  vector<double> vec_pred;
-  // Create a tree stucture
-    TTree *tree_spectrum = new TTree("tree_spectrum", "Meas and Pred Test");
-  tree_spectrum->Branch("vec_pred", &vec_pred);
-  tree_spectrum->Branch("vec_meas", &vec_meas);
-  // Create input structures
-  TString xpath = "input/presave_3v_hypothesis_toydata_01_cv.root";
-  TFile *inputfile_toydata_cv = new TFile(xpath, "read");
-  TTree *tree_toydata = (TTree*)inputfile_toydata_cv->Get("tree_toydata");
-  vector<double>  *vec_toydata_spectrum;
-  TBranch *b_vec_toydata_spectrum; //!
-  // Create output vectors for for the three outputs I need to create
-  vector<double> vec_chi2_3v;
-  vector<double> vec_chi2_4v;
-  vector<double> vec_dchi2_obs;
   idm2 = 41;
   it14 = 30;
   // Create arrays to hold the values I need
@@ -276,30 +160,83 @@ int contour = 0;
   val_obj_dm2 = pow(10, val_obj_dm2);
   double val_obj_ttt = h1d_ttt->GetBinCenter(it14);
   val_obj_ttt = pow(10, val_obj_ttt);
-  double grid_4v[4] = {val_obj_dm2, val_obj_ttt, 0, 0};
+    double pars_4v_grid[4] ={val_obj_dm2, val_obj_ttt, 0.0045, 0};
 
-  // Load xiangpan tree
-  tree_toydata->SetBranchAddress("vec_toydata_spectrum", &vec_toydata_spectrum, &b_vec_toydata_spectrum);
-  int entries = tree_toydata->GetEntries();
-  cout << endl << " ---> reading tree_toydata, entries " << entries << endl;
+// --------------------------------------------------
+// Open input file
+// --------------------------------------------------
+TString xpath = "input/presave_3v_hypothesis_toydata_01_cv.root";
+TFile* inputfile_toydata_cv = TFile::Open(xpath, "READ");
 
-  int(i = 79; i < 10; i++) {
-    tree_toydata->GetEntry(i);
-    for (int idx = 0; idx <= 182; idx++) {
-      double content = vec_toydata_spectrum->at(idx);
-      osc_test->matrix_tosc_fitdata_newworld(0, idx) = content;
+if (!inputfile_toydata_cv || inputfile_toydata_cv->IsZombie()) {
+    std::cerr << "Error: cannot open input file\n";
+    return 1;
+}
+
+// --------------------------------------------------
+// Get tree
+// --------------------------------------------------
+TTree* tree_toydata =
+    (TTree*)inputfile_toydata_cv->Get("tree_toydata");
+
+if (!tree_toydata) {
+    std::cerr << "Error: tree_toydata not found\n";
+    return 1;
+}
+
+// --------------------------------------------------
+// TTreeReader setup (SAFE ROOT I/O)
+// --------------------------------------------------
+TTreeReader reader(tree_toydata);
+
+// This replaces SetBranchAddress entirely
+TTreeReaderValue<vector<double>> vec_toydata_spectrum(
+    reader, "vec_toydata_spectrum"
+);
+
+// --------------------------------------------------
+// Your output containers
+// --------------------------------------------------
+vector<double> vec_chi2_3v;
+
+// --------------------------------------------------
+// Index map (cache)
+// --------------------------------------------------
+vector<vector<double>> spectrum_cache;
+spectrum_cache.reserve(200);  // optional optimization
+
+while (reader.Next()) {
+    spectrum_cache.push_back(*vec_toydata_spectrum);
+}
+
+cout << "Cached entries: " << spectrum_cache.size() << endl;
+// --------------------------------------------------
+// Loop over entries
+// --------------------------------------------------
+int entry_index = 0;
+	  // TMatrixD matrix_delta_3vPred_data   = matrix_toydata_temp - matrix_3v_asmiov_pred;
+	  // TMatrixD matrix_delta_3vPred_data_T = matrix_delta_3vPred_data.T(); matrix_delta_3vPred_data.T();
+	  // double chi2_3v_with_data = (matrix_delta_3vPred_data * matrix_3v_total_COV_inv * matrix_delta_3vPred_data_T)(0,0);
+
+for (size_t i = 0; i < 200; i++) {
+
+    auto& spectrum = spectrum_cache[i];
+    for (int j = 0; j <= 181; j++) {
+        osc_test->matrix_tosc_fitdata_newworld(0, j) = spectrum[j];
     }
-    // Fill out values for the chi2 values
-    double chi2_3v = osc_test->FCN(pars_3v_small);
-    vec_chi2_3v.push_back(chi2_3v);
-  }
+
+    // double chi2 = osc_test->FCN(pars_3v_small);
+    double chi2 = osc_test->FCN(pars_4v_grid);
+cout << chi2 << endl;
+    vec_chi2_3v.push_back(chi2);
+}
+
+cout << "dm2 " << val_obj_dm2 << "ttt " << val_obj_ttt << endl;
 
 
-  TFile outfile("output/jesse_dm2_41_ttt_30.root", "RECREATE");
+  TFile outfile("output/random_test.root", "RECREATE");
   TTree *tree = new TTree("tree", "Obs Test");
-  // tree->Branch("vec_chi2_4v", &vec_chi2_4v);
   tree->Branch("vec_chi2_3v", &vec_chi2_3v);
-  // tree->Branch("vec_dchi2_obs", &vec_dchi2_obs);
   tree->Fill();
   tree->Write();
   outfile.Close();
