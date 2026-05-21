8ef8df9c3cc614d648e53dd8e2aed13c6e0f996b
Jesse Mendez <jessepmendez79@gmail.com>
2026-05-20 19:20:17 -0500

    Started writing this to output the final cls map

---
+++ write_cls.cxx
@@ -0,0 +1,213 @@
+#include<iostream>
+#include<fstream>
+#include<sstream>
+#include<cmath>
+#include "stdlib.h"
+using namespace std;
+
+#include<vector>
+#include<map>
+#include<set>
+
+#include "WCPLEEANA/TOsc.h"
+
+#include "TTreeReader.h"
+#include "TTreeReaderValue.h"
+#include "WCPLEEANA/Configure_Osc.h"
+
+#include "TApplication.h"
+#include "TParameter.h"
+#include <chrono> // timer
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////// MAIN //////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+int main(int argc, char** argv)
+{
+  TString roostr = "";
+
+  cout<<endl<<" ---> A Hello story ..."<<endl<<endl;
+
+
+
+
+  double scaleF_POT_BNB  = 1;
+  double scaleF_POT_NuMI = 1;
+  int display = 0;
+
+
+  int it14 = 0;
+  int idm2 = 0;
+
+
+  const int NUM_dm2 = 60;
+  const int NUM_ttt = 60;
+
+  TH1D *h1d_dm2 = new TH1D("h1d_dm2", "h1d_dm2", NUM_dm2, -2, 1);
+  TH1D *h1d_ttt = new TH1D("h1d_ttt", "h1d_ttt", NUM_ttt, -2, 0);
+
+
+
+  // double it14 = 0;
+  // double idm2 = 0;
+  // double it24 = 0;
+
+
+  if( !display ) {
+    gROOT->SetBatch( 1 );
+  }
+
+  TApplication theApp("theApp",&argc,argv);
+
+  /////////////////////////////////////////////////////////// Draw style
+
+
+  ///////////////////////////////////////////////////////////
+
+  TOsc *osc_test = new TOsc();
+
+  ///////
+
+  osc_test->tosc_scaleF_POT_BNB  = scaleF_POT_BNB;
+  osc_test->tosc_scaleF_POT_NuMI = scaleF_POT_NuMI;
+
+  ///////
+
+  osc_test->flag_apply_oscillation_BNB  = Configure_Osc::flag_apply_oscillation_BNB;
+  osc_test->flag_apply_oscillation_NuMI = Configure_Osc::flag_apply_oscillation_NuMI;
+
+  osc_test->flag_goodness_of_fit_CNP    = Configure_Osc::flag_goodness_of_fit_CNP;
+
+  ///////
+
+  osc_test->flag_syst_dirt   = Configure_Osc::flag_syst_dirt;
+  osc_test->flag_syst_mcstat = Configure_Osc::flag_syst_mcstat;
+  osc_test->flag_syst_flux   = Configure_Osc::flag_syst_flux;
+  osc_test->flag_syst_geant  = Configure_Osc::flag_syst_geant;
+  osc_test->flag_syst_Xs     = Configure_Osc::flag_syst_Xs;
+  osc_test->flag_syst_det    = Configure_Osc::flag_syst_det;
+
+  ///////
+
+  osc_test->flag_NuMI_nueCC_from_intnue         = Configure_Osc::flag_NuMI_nueCC_from_intnue;
+  osc_test->flag_NuMI_nueCC_from_overlaynumu    = Configure_Osc::flag_NuMI_nueCC_from_overlaynumu;
+  osc_test->flag_NuMI_nueCC_from_appnue         = Configure_Osc::flag_NuMI_nueCC_from_appnue;
+  osc_test->flag_NuMI_nueCC_from_appnumu        = Configure_Osc::flag_NuMI_nueCC_from_appnumu;
+  osc_test->flag_NuMI_nueCC_from_overlaynueNC   = Configure_Osc::flag_NuMI_nueCC_from_overlaynueNC;
+  osc_test->flag_NuMI_nueCC_from_overlaynumuNC  = Configure_Osc::flag_NuMI_nueCC_from_overlaynumuNC;
+
+  osc_test->flag_NuMI_numuCC_from_overlaynumu   = Configure_Osc::flag_NuMI_numuCC_from_overlaynumu;
+  osc_test->flag_NuMI_numuCC_from_overlaynue    = Configure_Osc::flag_NuMI_numuCC_from_overlaynue;
+  osc_test->flag_NuMI_numuCC_from_appnue        = Configure_Osc::flag_NuMI_numuCC_from_appnue;
+  osc_test->flag_NuMI_numuCC_from_appnumu       = Configure_Osc::flag_NuMI_numuCC_from_appnumu;
+  osc_test->flag_NuMI_numuCC_from_overlaynumuNC = Configure_Osc::flag_NuMI_numuCC_from_overlaynumuNC;
+  osc_test->flag_NuMI_numuCC_from_overlaynueNC  = Configure_Osc::flag_NuMI_numuCC_from_overlaynueNC;
+
+  osc_test->flag_NuMI_CCpi0_from_overlaynumu    = Configure_Osc::flag_NuMI_CCpi0_from_overlaynumu;
+  osc_test->flag_NuMI_CCpi0_from_appnue         = Configure_Osc::flag_NuMI_CCpi0_from_appnue;
+  osc_test->flag_NuMI_CCpi0_from_overlaynumuNC  = Configure_Osc::flag_NuMI_CCpi0_from_overlaynumuNC;
+  osc_test->flag_NuMI_CCpi0_from_overlaynueNC   = Configure_Osc::flag_NuMI_CCpi0_from_overlaynueNC;
+
+  osc_test->flag_NuMI_NCpi0_from_overlaynumu    = Configure_Osc::flag_NuMI_NCpi0_from_overlaynumu;
+  osc_test->flag_NuMI_NCpi0_from_appnue         = Configure_Osc::flag_NuMI_NCpi0_from_appnue;
+  osc_test->flag_NuMI_NCpi0_from_overlaynumuNC  = Configure_Osc::flag_NuMI_NCpi0_from_overlaynumuNC;
+  osc_test->flag_NuMI_NCpi0_from_overlaynueNC   = Configure_Osc::flag_NuMI_NCpi0_from_overlaynueNC;
+
+
+  ///////
+
+  osc_test->flag_BNB_nueCC_from_intnue         = Configure_Osc::flag_BNB_nueCC_from_intnue;
+  osc_test->flag_BNB_nueCC_from_overlaynumu    = Configure_Osc::flag_BNB_nueCC_from_overlaynumu;
+  osc_test->flag_BNB_nueCC_from_appnue         = Configure_Osc::flag_BNB_nueCC_from_appnue;
+  osc_test->flag_BNB_nueCC_from_appnumu        = Configure_Osc::flag_BNB_nueCC_from_appnumu;
+  osc_test->flag_BNB_nueCC_from_overlaynueNC   = Configure_Osc::flag_BNB_nueCC_from_overlaynueNC;
+  osc_test->flag_BNB_nueCC_from_overlaynumuNC  = Configure_Osc::flag_BNB_nueCC_from_overlaynumuNC;
+
+  osc_test->flag_BNB_numuCC_from_overlaynumu   = Configure_Osc::flag_BNB_numuCC_from_overlaynumu;
+  osc_test->flag_BNB_numuCC_from_overlaynue    = Configure_Osc::flag_BNB_numuCC_from_overlaynue;
+  osc_test->flag_BNB_numuCC_from_appnue        = Configure_Osc::flag_BNB_numuCC_from_appnue;
+  osc_test->flag_BNB_numuCC_from_appnumu       = Configure_Osc::flag_BNB_numuCC_from_appnumu;
+  osc_test->flag_BNB_numuCC_from_overlaynumuNC = Configure_Osc::flag_BNB_numuCC_from_overlaynumuNC;
+  osc_test->flag_BNB_numuCC_from_overlaynueNC  = Configure_Osc::flag_BNB_numuCC_from_overlaynueNC;
+
+  osc_test->flag_BNB_CCpi0_from_overlaynumu    = Configure_Osc::flag_BNB_CCpi0_from_overlaynumu;
+  osc_test->flag_BNB_CCpi0_from_appnue         = Configure_Osc::flag_BNB_CCpi0_from_appnue;
+  osc_test->flag_BNB_CCpi0_from_overlaynumuNC  = Configure_Osc::flag_BNB_CCpi0_from_overlaynumuNC;
+  osc_test->flag_BNB_CCpi0_from_overlaynueNC   = Configure_Osc::flag_BNB_CCpi0_from_overlaynueNC;
+
+  osc_test->flag_BNB_NCpi0_from_overlaynumu    = Configure_Osc::flag_BNB_NCpi0_from_overlaynumu;
+  osc_test->flag_BNB_NCpi0_from_appnue         = Configure_Osc::flag_BNB_NCpi0_from_appnue;
+  osc_test->flag_BNB_NCpi0_from_overlaynumuNC  = Configure_Osc::flag_BNB_NCpi0_from_overlaynumuNC;
+  osc_test->flag_BNB_NCpi0_from_overlaynueNC   = Configure_Osc::flag_BNB_NCpi0_from_overlaynueNC;
+
+  /////// set only one time
+  /////// set only one time
+
+  osc_test->Set_default_cv_cov(Configure_Osc::default_cv_file,
+                               Configure_Osc::default_dirtadd_file,
+                               Configure_Osc::default_mcstat_file,
+                               Configure_Osc::default_fluxXs_dir,
+                               Configure_Osc::default_detector_dir);
+  osc_test->Set_oscillation_base(Configure_Osc::default_eventlist_dir);
+
+  for(int i=1; i<argc; i++) {
+    if( strcmp(argv[i],"-it14")==0 ) {
+      stringstream convert( argv[i+1] );
+      if(  !( convert>>it14 ) ) { cerr<<" ---> Error it14 !"<<endl; exit(1); }
+    }
+    if( strcmp(argv[i],"-idm2")==0 ) {
+      stringstream convert( argv[i+1] );
+      if(  !( convert>>idm2 ) ) { cerr<<" ---> Error idm2 !"<<endl; exit(1); }
+    }
+  }
+
+
+  // Create arrays to hold the values I need
+  int ittt = it14;
+  double pars_3v_small[4] = {0, 0.10, 0.11, 0};
+  double val_obj_dm2 = h1d_dm2->GetBinCenter(idm2);
+  val_obj_dm2 = pow(10, val_obj_dm2);
+  double val_obj_ttt = h1d_ttt->GetBinCenter(ittt);
+  val_obj_ttt = pow(10, val_obj_ttt);
+  double pars_4v_grid[4] ={val_obj_dm2, val_obj_ttt, 0.0045, 0};
+
+
+// Branch-backed pointers
+    std::vector<double>* invec4v = nullptr;
+    std::vector<double>* invec3v = nullptr;
+
+    // Your independent copies
+    std::vector<std::vector<double>> vec4v;
+    std::vector<std::vector<double>> vec3v;
+
+
+    roostr = TString::Format("output/out_dm2_ttt_%03d_%03d.root", idm2, it14);
+    // Scope for ROOT objects
+    TFile file(roostr, "READ");
+
+    TTree* tree = (TTree*)file.Get("tree");
+
+    tree->SetBranchAddress("vec_dchi2_4v", &invec3v);
+    tree->SetBranchAddress("vec_dchi2_3v", &invec4v);
+
+    tree->GetEntry(0);
+
+    vec4v.push_back(*invec4v);
+    vec3v.push_back(*invec3v);
+
+    file.Close();
+
+
+
+  cout << " ---> Finished successfully" << endl;
+
+  cout<<endl;
+  if( display ) {
+    cout<<" Enter Ctrl+c to end the program"<<endl;
+    cout<<" Enter Ctrl+c to end the program"<<endl;
+    cout<<endl;
+    theApp.Run();
+  }
+
+  return 0;
+}
