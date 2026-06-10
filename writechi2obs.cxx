442511b6bc5f3987b09ff47a6a4b57dd13a6a8e8
Jesse Mendez <jessepmendez79@gmail.com>
2026-06-10 11:54:50 -0500

    Edited to do the next round 4k-6k

--- writechi2obs.cxx
+++ writechi2obs.cxx
@@ -41,7 +41,7 @@ int main(int argc, char** argv)
     }
   }
 
-TString final_name = TString::Format("output/start_2k_size2k-2k_cls_out_dm2_ttt_%03d_%03d.root", idm2, it14);
+TString final_name = TString::Format("output/start_4k_size4k-6k_cls_out_dm2_ttt_%03d_%03d.root", idm2, it14);
 
 TString tmp_name = final_name + ".tmp";
 
@@ -246,8 +246,8 @@ tree->GetEntry(0);
 // const int size = 1000;/* number of elements to copy */
 
 
-constexpr std::size_t start = 2000;
-constexpr std::size_t count = 2000;
+constexpr std::size_t start = 4000;
+constexpr std::size_t count = 6000;
 
 // COPY OUT OF ROOT OWNERSHIP
 std::vector<double> vec4v(invec4v->begin() + start,
