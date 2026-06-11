a78121dbdee273cb1714db6e08b3c89e95eee617
Jesse Mendez <jessepmendez79@gmail.com>
2026-06-11 11:37:36 -0500

    Changed to look at the 2k sized distributions

--- analyzecls.cxx
+++ analyzecls.cxx
@@ -232,7 +232,7 @@ int main(void) {
 
       int idx = idm2 * N_it14 + it14;
 
-      TString roostr = TString::Format("output/size_1k_cls_out_dm2_ttt_%03d_%03d.root", idm2+1, it14+1);
+      TString roostr = TString::Format("output/start_2k_size2k-2k_cls_out_dm2_ttt_%03d_%03d.root", idm2+1, it14+1);
       TFile file(roostr, "READ");
 
       TTree* tree = (TTree*)file.Get("tree");
